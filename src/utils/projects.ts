0bf85b9655022e12686bc30af63209d97928069a
BwambokTanui <greenfontdomice@gmail.com>
2026-05-10 11:39:35 +0300

    Add Ddrumist project (Next.js, Supabase, TailwindCSS)

--- src/utils/projects.ts
+++ src/utils/projects.ts
@@ -2,16 +2,7 @@ import shyro from "../assets/shyro/shyro.png";
 import twosister from "../assets/twosister/twosister.png";
 import eFarm from "../assets/e-farm/e-farm.png";
 import kipsHomes from "../assets/kipshomes/KipsHomes.png";
-
-/*
- * To add a new project:
- * 1. Create a folder in src/assets/<project-name>/
- * 2. Add your screenshot there
- * 3. Import the image below
- * 4. Add an entry to the `projects` object
- */
-
-// import newProjectImg from "../assets/new-project/new-project.png";
+import ddrumist from "../assets/ddrumist/ddrumist.png";
 
 const projects = {
   shyro: {
@@ -34,11 +25,11 @@ const projects = {
     techs: ["FlutterFlow", "Firebase"],
     images: [kipsHomes],
   },
-  // newProject: {
-  //   name: "My New Project",
-  //   techs: ["React", "Node"],
-  //   images: [newProjectImg],
-  // },
+  ddrumist: {
+    name: "Ddrumist",
+    techs: ["Next.js", "Supabase", "TailwindCSS"],
+    images: [ddrumist],
+  },
 };
 
 const projectList = Object.entries(projects).map(([key, val]) => ({
